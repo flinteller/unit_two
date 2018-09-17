fac07212367493a49146e934185b707f0fc0d2ea
Flint <flint@iMac1.local>
2018-09-17 15:08:43 -0400

    Wrote up to favorite number in flowchart

---
+++ assignment_two.py
@@ -0,0 +1,18 @@
+# Flint Eller
+# 9/21/18
+# This is a chatbot that chats about, names, places, favorite, numbers, amd cars.
+
+print("Hello there!")
+
+user_name = input("What is your name?")
+
+print("Well hey there", user_name , "I'm Pixel, a chatbot created by Flint")
+
+user_place = input("Where are you from?")
+
+print("Cool,", user_place, "sounds like a great place!")
+
+pixel_fav_number = 10
+
+user_fav_number = input("What is your favorite number?")
+
