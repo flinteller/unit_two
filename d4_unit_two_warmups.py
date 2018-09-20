6d54d2799a644aa97ff7bf7bcd334be86227cd29
Flint <flint@iMac1.local>
2018-09-20 09:15:52 -0400

    Looking at different math operators

---
+++ d4_unit_two_warmups.py
@@ -0,0 +1,22 @@
+print(9 * 5)
+
+print(2 / 5)
+
+print(12 // 5)
+
+print(27 // 4)
+
+print(2 // 5)
+
+print(5 % 2)
+
+print(9 % 5)
+
+print(6 % 6)
+
+print(2 % 7)
+
+# I think the * does multiplication
+# I think the / does division
+# I think the // gives the number of time one goes into the other one as an integer 
+# I think % gives the remainder after division
